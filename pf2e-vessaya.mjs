a4fcdb8e4083802b72099985c946793979428efe
Dale Emasiri <demasiri@gmail.com>
2025-02-18 12:20:55 +0100

    some exploration of data objects

--- pf2e-vessaya.mjs
+++ pf2e-vessaya.mjs
@@ -6,3 +6,24 @@ Hooks.once("init", () => {
 	module.opts = {}
 	module.dataModels = {}
 })
+
+Hooks.once("ready", async () => {
+	const currentVersion = game.modules.get("pf2e-vessaya").version
+	const lastVersion = game.modules.get("pf2e-vessaya", "lastVersion")
+
+	console.log(currentVersion, lastVersion)
+
+	if (foundry.utils.isNewerVersion(currentVersion, lastVersion)) {
+		// Do some logic here
+	}
+
+	const pf2e = CONFIG.PF2E
+
+	// Preserve the original languages
+	let pf2elang = pf2e.languages
+
+	// How we do this...
+	// We set up a bunch of languages, then...
+	// in game.pf2e.settings.campaign.languages.common
+	// we = [ "langs" ]
+})
