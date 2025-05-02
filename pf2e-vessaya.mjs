c5db63ba036f6cbf93334947f9fed1f6720c1b13
dade <demasiri@gmail.com>
2025-05-02 16:27:53 +0200

    Started conversion of content to V13 compatibility

--- pf2e-vessaya.mjs
+++ pf2e-vessaya.mjs
@@ -1,8 +1,9 @@
 import { LANGUAGES_BY_RARITY, REPUTATION } from "./scripts/consts.mjs"
-// import { default as VessayaJournalSheet } from "./scripts/journal/journal-sheet.mjs"
-import { VessayaJournalSheet } from "./scripts/journal/journal-sheet.mjs"
+import { default as VessayaJournalSheet } from "./scripts/journal/journal-sheet.mjs"
+// import { VessayaJournalSheet } from "./scripts/journal/journal-sheet.mjs"
 import { initConfigs } from "./scripts/configs.mjs"
 import { ReputationSystem } from "./scripts/reputation/reputation-system.mjs"
+import { Enrichers } from "./scripts/journal/enrichers.mjs"
 
 const MODULE = "pf2e-vessaya"
 
@@ -29,7 +30,7 @@ async function updateSource(source, langs) {
 	await game.settings.set("pf2e", "homebrew.languageRarities", source)
 }
 
-Hooks.once("init", () => {
+Hooks.once("init", async () => {
 	globalThis.vessaya = game.modules.get(MODULE)
 	vessaya.opts = {}
 	vessaya.dataModels = {}
@@ -43,6 +44,8 @@ Hooks.once("init", () => {
 
 	initConfigs()
 	ReputationSystem.init()
+
+	// NOTE: Enrichers are declared and handled further down in a second Hooks.once("init") call
 })
 
 Hooks.once("ready", async () => {
@@ -61,23 +64,28 @@ Hooks.once("ready", async () => {
 		ReputationSystem.repopulateData(game.actors.party, vessaya.rep, true)
 })
 
-Hooks.on("renderJournalSheet", () => {
+Hooks.on("renderJournalSheet", (app, html) => {
+	console.log(app, html)
 	// horribly hacky jquery code... don't look at it. i'm ashamed... :(
-	const el = $(".sheet.journal-sheet.vessaya .document-title-edit")
-	if (el.length > 0) {
-		el.delete()
-		$('.sheet.journal-sheet.vessaya .window-header .document-id-link').after(
-			`<a class="document-title-edit" data-tooltip="Edit Document Title" data-tooltip-direction="UP"><i class="fa-solid fa-edit"></i></a>`
-		)
-	} else {
-		$('.sheet.journal-sheet.vessaya .window-header .document-id-link').after(
-			`<a class="document-title-edit" data-tooltip="Edit Document Title" data-tooltip-direction="UP"><i class="fa-solid fa-edit"></i></a>`
-		)
-	}
+	const el = html.find(".window-title")
+	console.log(el)
+	// if (el.length === 0) {
+	// 	$('.sheet.journal-sheet.vessaya .window-header .document-id-link').after(
+	// 		`<a class="document-title-edit" data-tooltip="Edit Document Title" data-tooltip-direction="UP"><i class="fa-solid fa-edit"></i></a>`
+	// 	)
+	// } else {
+	// }
+	//
+	// $('.sheet.journal-sheet.vessaya .document-title-edit').on("click", () => {
+	// 	$(".sheet.journal-sheet.vessaya input.title").toggleClass("display")
+	// })
+})
 
-	$('.sheet.journal-sheet.vessaya .document-title-edit').click(() => {
-		$(".sheet.journal-sheet.vessaya input.title").toggleClass("display")
-	})
+Hooks.on("renderJournalPageSheet", (app, html) => {
+	const doc = app.document
+	const journalFlag = doc.parent.sheet instanceof VessayaJournalSheet
+	if (journalFlag)
+		html.addClass("vessaya")
 })
 
 Hooks.on("renderPartySheetPF2e", function(sheet, html, data) {
@@ -235,4 +243,9 @@ Hooks.on("renderPartySheetPF2e", function(sheet, html, data) {
 			}, 500)
 		})
 	})
+})
+
+Hooks.once("init", async () => {
+	Enrichers.initViewScene()
+	$(document).on("click", ".viewscene", Enrichers.viewScene)
 })
